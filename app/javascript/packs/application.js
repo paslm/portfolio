8939394d4d51d7884e3157a0cf2a458ac3c3564f
Mirchev Pascal <pascalmirchev2000@gmail.com>
2021-06-17 11:34:06 -0400

    This is finally done, the bootsrap setup is functional. I fixed the Jquery problem by adding the line at the top of application.html.erb page. It's not the proper rails way but a man gotta work so....

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("channels")
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//= require popper
+//= require jquery3
+//= require bootstrap-sprockets
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("channels")
